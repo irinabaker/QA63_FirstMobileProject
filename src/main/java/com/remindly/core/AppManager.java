acdd47ab37022b759462ce8e7287d78b1b83dea6
Ira Baker <49585685+irinabaker@users.noreply.github.com>
2025-11-09 12:41:59 +0100

    Update AppManager.java

--- src/main/java/com/remindly/core/AppManager.java
+++ src/main/java/com/remindly/core/AppManager.java
@@ -28,7 +28,7 @@ public class AppManager {
         capabilities.setCapability("appActivity","com.blanyal.remindme.MainActivity");
         capabilities.setCapability("app","C:/mobile/wikipedia.apk");
 
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723"),capabilities);
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
         mainScreen = new MainScreenHelper(driver);
