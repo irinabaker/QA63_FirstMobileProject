cd8fa920f70fd1c26b185a203d2e9b3687fc052d
baker <paxpistor@gmail.com>
2025-11-10 06:42:50 +0100

    Updated

--- src/main/java/com/remindly/core/AppManager.java
+++ src/main/java/com/remindly/core/AppManager.java
@@ -26,7 +26,7 @@ public class AppManager {
         capabilities.setCapability("deviceName","mob");
         capabilities.setCapability("appPackage","com.blanyal.remindly");
         capabilities.setCapability("appActivity","com.blanyal.remindme.MainActivity");
-        capabilities.setCapability("app","C:/mobile/wikipedia.apk");
+        capabilities.setCapability("app","C:/mobile/remindly.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
